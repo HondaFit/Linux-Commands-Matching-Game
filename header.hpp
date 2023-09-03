e43929c1c5ae832c99e39519ee93d4100e9ab38b
Harish <harish.s.sri@gmail.com>
2023-09-02 19:50:15 -0700

    adding header info

--- header.hpp
+++ header.hpp
@@ -1,4 +1,31 @@
-hello test
+#define _CRT_SECURE_NO_WARNINGS
+#define SFML_NO_DEPRECATED_WARNINGS
 
-hello 
-hello
+#pragma once
+
+#include <iostream>
+#define SPACE 10
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <iomanip>
+
+
+using std::cout; // we can now use cout without explicitly writing std::cout
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::toupper;
+using std::getline;
+using std::vector;
+using std::put_time;
+using std::localtime;
+
+using std::stoi;
