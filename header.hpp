76c0893a9bcd931614df2bad9edb204055ca582b
Harish <harish.s.sri@gmail.com>
2023-09-17 12:55:12 -0700

    Added game to menu. As well as some bug fixes

--- header.hpp
+++ header.hpp
@@ -10,11 +10,13 @@
 #include <ctime>
 #include <cmath>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <chrono>
 #include <algorithm>
 #include <iomanip>
 #include <cctype>
+#include <random>
 
 using std::cin;
 using std::cout; // we can now use cout without explicitly writing std::cout
@@ -91,19 +93,36 @@ public:
     {
         loadCommands();
         loadProfiles();
+        totalCount = countList();
+        // system("clear");
     }
 
-    void readList()
+    int readList()
     {
+        int count = 1;
         node<T1, T2> *pCurrent = pHead;
         while (pCurrent != NULL)
         {
-            cout << pCurrent->getCom() << "," << pCurrent->getDesc() << endl;
+            cout << count << ") " << pCurrent->getCom() << "," << pCurrent->getDesc() << endl;
             pCurrent = pCurrent->getNext();
+            count++;
         }
+        return count;
+    }
+    int countList()
+    {
+        int count = 1;
+        node<T1, T2> *pCurrent = pHead;
+        while (pCurrent->getNext() != NULL)
+        {
+            // cout << count << ") " << pCurrent->getCom() << "," << pCurrent->getDesc() << endl;
+            pCurrent = pCurrent->getNext();
+            count++;
+        }
+        return count;
     }
 
-    void menu()
+    void menu() // MAIN FUNCTION
     {
         int exit = false;
         while (exit == false)
@@ -111,7 +130,7 @@ public:
 
             switch (menuSelection())
             {
-            case 1:
+            case 1: // done
                 system("clear");
                 cout << "Selected Game Rules" << endl;
                 displayRules();
@@ -120,22 +139,25 @@ public:
             case 2:
                 system("clear");
                 cout << "Selected Play Game" << endl;
+                game();
                 break;
 
             case 3:
-                system("clear");
+                system("clear"); // done
                 cout << "Selected Load Previous Game" << endl;
                 selectProfile();
                 break;
 
             case 4:
-                system("clear");
+                system("clear"); // done
                 cout << "Selected Add Command" << endl;
+                addCommand();
                 break;
 
             case 5:
-                system("clear");
+                system("clear"); // done
                 cout << "Selected Remove Command" << endl;
+                removeCommand();
                 break;
 
             case 6:
@@ -147,16 +169,54 @@ public:
         }
     }
 
+    void game()
+    {
+
+        bool exit = false;
+        int randCommand = 0;
+        char dummy;
+        while (profiles[currentProfile - 1] <= 30 || exit == false)
+        {
+            randCommand = generateRandomNumber(totalCount);
+            // cout << randCommand << endl;
+            comFinder(randCommand);
+            problemMaker(randCommand);
+            if (answerQuestion() == true)
+            {
+                cout << "\nCorrect, You got the Answer Correct! You get a Point!" << endl;
+                profiles[currentProfile - 1]++;
+            }
+            else
+            {
+                cout << "\nIncorrect, You got the Answer Wrong. Minus 1 Point" << endl;
+                profiles[currentProfile - 1]--;
+            }
+            cout << "Press any key to continue" << endl;
+            cin >> dummy;
+            system("clear");
+        }
+    }
+
+    int generateRandomNumber(int max)
+    {
+        return (std::rand() % max) + 1;
+    }
+
 private:
     ifstream listFile;
     ifstream playerFile;
     ifstream ruleFile;
 
     vector<int> profiles;
-    int currentProfile; // does not give score but index of the vector profiles indicating who is curretnly playing
+    int currentProfile = 1; // does not give score but index of the vector profiles indicating who is curretnly playing
 
     node<T1, T2> *pHead = NULL;
     node<T1, T2> *pCurrent = NULL;
+
+    int totalCount;
+    vector<string> possibleAnswer;
+    string correctCommand;
+    string correctDescription;
 
     void loadCommands()
     {
@@ -214,8 +274,8 @@ private:
 
             if (choice != "1" && choice != "2" && choice != "3" && choice != "4" && choice != "5" && choice != "6")
             {
-                cout << "incorrect input try again" << endl;
-                cout << "Press any key to continue" << endl;
+                cout << "\nincorrect input try again" << endl;
+                cout << "Press any key to continue";
                 cin >> dummy;
                 system("clear");
             }
@@ -284,7 +344,141 @@ private:
         system("clear");
     }
 
-    bool digitChecker(string userInput) //checkin gif user typed in anything other than numbers
+    void addCommand()
+    {
+        char dummy;
+        int exit = false;
+        string command;
+        string description;
+
+        while (exit == false)
+        {
+            cout << "Type in a new command (no space at end): " << endl;
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            getline(cin, command);
+            if (command[command.length() - 1] == ' ')
+            {
+                cout << "\nYou have a space at the end of your command. Retype it" << endl;
+                cout << "Press any key to continue  ";
+                cin >> dummy;
+                system("clear");
+            }
+            else
+            {
+                exit = true;
+            }
+        }
+        exit = false;
+
+        while (exit == false)
+        {
+            cout << "Type in a descroption of your new command (no space in the front): ";
+            // cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            getline(cin, description);
+            if (description[0] == ' ')
+            {
+                cout << "\nYou have a space at the front of your description. Retype it" << endl;
+                cout << "Press any key to continue  ";
+                cin >> dummy;
+                system("clear");
+            }
+            else
+            {
+                exit = true;
+            }
+        }
+
+        node<T1, T2> *pTemp;
+        pTemp = new node<T1, T2>;
+        pTemp->setComDesc(command, description); // assigning command and desciption to the new node
+
+        if (pHead != NULL) // If head is not empty
+        {
+            pTemp->setNext(pHead);
+            pHead = pTemp;
+        }
+        else // if head is empty
+        {
+            pHead = pTemp;
+        }
+
+        cout << "Press any key to continue";
+        cin >> dummy;
+        system("clear");
+    }
+    void removeCommand()
+    {
+        char dummy;
+        string userInput;
+        int count;
+        int exit = false;
+
+        while (exit == false)
+        {
+            count = readList();
+            cout << "\nSelect a Command you want to Delete" << endl;
+            cin >> userInput;
+
+            if (digitChecker(userInput) == true)
+            {
+                if (stoi(userInput) >= 1 && stoi(userInput) <= count)
+                {
+                    exit = true;
+                }
+                else
+                {
+                    cout << "\nInvalid Selection, Press any key to continue ";
+                    cin >> dummy;
+                    system("clear");
+                }
+            }
+            else
+            {
+                cout << "\nInvalid Selection, Press any key to continue ";
+                cin >> dummy;
+                system("clear");
+            }
+        }
+
+        totalCount = count;
+        count = stoi(userInput); // repurposing count. It is now the "index" that needs to be deleted
+
+        node<T1, T2> *pPrev;
+        node<T1, T2> *pCurrent;
+        if (count == 1) // needs to be deleted at the head
+        {
+            pCurrent = pHead; // acting as a temp pointer
+            pHead = pHead->getNext();
+            delete pCurrent;
+        }
+        else if (count == totalCount) // if we need to delete at the end of the list
+        {
+            while (pCurrent->getNext() != NULL)
+            {
+                pCurrent = pCurrent->getNext();
+            }
+            delete pCurrent->getNext();
+            pCurrent->setNext(NULL);
+        }
+        else
+        {
+            pPrev = pHead;
+            pCurrent = pHead->getNext();
+            for (int i = 2; i < count; i++)
+            {
+                pPrev = pCurrent;
+                pCurrent = pCurrent->getNext();
+            }
+
+            pPrev->setNext(pCurrent->getNext());
+            delete pCurrent;
+        }
+
+        system("clear");
+        totalCount = countList();
+    }
+
+    bool digitChecker(string userInput) // checkin gif user typed in anything other than numbers
     {
         bool isAlpha = false;
         for (long unsigned int i = 0; i < userInput.length(); i++)
@@ -301,6 +495,143 @@ private:
         else
         {
             return true;
+        }
+    }
+
+    void comFinder(int number)
+    {
+        node<T1, T2> *pCurrent = pHead;
+        for (int i = 1; i < number; i++)
+        {
+            pCurrent = pCurrent->getNext();
+        }
+        
+        if(pCurrent != NULL)
+        {
+    correctCommand = pCurrent->getCom();
+        correctDescription = pCurrent->getDesc();
+        }
+        else
+        {
+            cout << "comFinder pCurrent is NULL!" << endl;
+        }
+
+        // cout << pCurrent->getCom() << endl;
+    }
+
+    string descFinder(int desc)
+    {
+
+        node<T1, T2> *pCurrent = pHead;
+        for (int i = 1; i < desc; i++)
+        {
+            pCurrent = pCurrent->getNext();
+        }
+
+        if(pCurrent !=NULL)
+        {
+        return pCurrent->getDesc();
+        }
+        else
+        {
+            cout << "descFinder pCurrent is NULL   " <<desc << endl;
+            return NULL;
+        }
+    }
+
+    void problemMaker(int cAnswer)
+    {
+
+        int randomDesc;   // what desc we're trying to find
+        int descLocation; // where in the array it wil be stored
+
+        possibleAnswer.push_back(""); // making an array of strings
+        possibleAnswer.push_back("");
+        possibleAnswer.push_back("");
+        possibleAnswer.push_back("");
+
+        descLocation = generateRandomNumber(4);
+        possibleAnswer[descLocation - 1] = descFinder(cAnswer); // taking the correct answer and putting it somehwere in the arrray
+
+        for (int i = 0; i < 3; i++) // make the possible answers. This will include 3 wrong answers and 1 correct answer
+        {
+            randomDesc = generateRandomNumber(totalCount);
+
+            while (!possibleAnswer[descLocation - 1].empty()) // looking for an empty spot in the array
+            {
+                descLocation = generateRandomNumber(4);
+            }
+            possibleAnswer[descLocation - 1] = descFinder(randomDesc);
+        }
+
+        /*for (int i = 0; i < 4; i++)
+        {
+            cout << i + 1 << ") " << possibleAnswer[i] << endl;
+        }*/
+    }
+    void problemPrinter()
+    {
+
+        for (int i = 0; i < 4; i++)
+        {
+            cout << i + 1 << ") " << possibleAnswer[i] << endl;
+        }
+    }
+    bool answerQuestion()
+    {
+        char dummy;
+        string userInput;
+        int exit = false;
+
+        while (exit == false)
+        {
+            cout << "Current Points for Player " << currentProfile << ": " << profiles[currentProfile - 1] << endl;
+            cout << "Problem: What is the Description that best fits the Command: " << correctCommand << endl;
+            problemPrinter();
+            cout << "\nSelect your Description ";
+            cin >> userInput;
+
+            if (digitChecker(userInput) == true)
+            {
+                if (stoi(userInput) >= 1 && stoi(userInput) <= 4)
+                {
+                    exit = true;
+                }
+                else
+                {
+                    cout << "\nInvalid Selection, Press any key to continue ";
+                    cin >> dummy;
+                    system("clear");
+                }
+            }
+            else
+            {
+                cout << "\nInvalid Selection, Press any key to continue ";
+                cin >> dummy;
+                system("clear");
+            }
+        }
+
+        int selection = stoi(userInput);
+
+        node<T1, T2> *pCurrent = pHead;
+        while (pCurrent != NULL && pCurrent->getDesc() != possibleAnswer[selection - 1])
+        {
+            pCurrent = pCurrent->getNext();
+        }
+        if (pCurrent != NULL && pCurrent->getDesc() == correctDescription)
+        {
+            possibleAnswer.clear();
+            correctCommand.clear();
+            correctDescription.clear();
+            return true;
+        }
+        else
+        {
+            possibleAnswer.clear();
+            correctCommand.clear();
+            correctDescription.clear();
+            return false;
         }
     }
 };
--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 int main()
 {
 
+        std::srand(static_cast<unsigned int>(std::time(nullptr)));
     list<string,string> linkedList;
+    
 
     //linkedList.readList(); //used for viewing linked list
 
+    //linkedList.menu();
     linkedList.menu();
-    
+
     cout << "done" << endl;
     cout << "hello world"<<endl;
     return 0;
